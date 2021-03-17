e3471a86f36e41dfe8a66a6b920c752934e4dbd2
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2021-03-17 11:14:41 +0100

    dropDown navbar fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,53 +7,53 @@ import logo from "../pictures/logo_FDJ_FINAL_800.png"
 const Navbar = () => {
 
     const {data: festivals, error, isPending} = useFetch("http://localhost:8000/festivals")
-    const {data: currentFestival, setData: setCurrentFestival} = useFetch("http://localhost:8000/festivals/current")
+    const {data: selectedFestival, setData: setSelectedFestival} = useFetch("http://localhost:8000/festivals/current")
 
     const handleChange = (eventKey, event) => {
-        setCurrentFestival(JSON.parse(eventKey))
+        setSelectedFestival(JSON.parse(eventKey))
     }
 
     const handleClick = () => {
-        console.log(currentFestival)
+        console.log(selectedFestival)
     }
 
     return (
         <div>
             {isPending && <div>loading... </div>}
             {error && <div>{error}</div>}
-            {festivals && currentFestival &&
+            {festivals && selectedFestival &&
             <Row>
                 <Col md={2}>
                     <Image src={logo} fluid onClick={handleClick}/>
                 </Col>
-                <Col md={9}>
+                <Col md={2}>
+                    <Dropdown className="button">
+                        <Dropdown.Toggle id="dropdown-basic">
+                            {selectedFestival.name}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {festivals.map((festival, index) =>
+                                <Dropdown.Item as="button" value={festival} key={index} eventKey={JSON.stringify(festival)} onSelect={handleChange}>
+                                    {festival.name}
+                                </Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Col>
+                <Col md={8}>
                     <Nav fill variant="tabs" defaultActiveKey="/">
-                        <Nav.Item>
-                            <Dropdown>
-                                <Dropdown.Toggle id="dropdown-basic">
-                                    {currentFestival.name}
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    {festivals.map((festival, index) =>
-                                        <Dropdown.Item as="button" value={festival} key={index} eventKey={JSON.stringify(festival)} onSelect={handleChange}>
-                                            {festival.name}
-                                        </Dropdown.Item>
-                                    )}
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Nav.Item>
                         <Nav.Item>
                             <Link to="/" className='nav-link'>Dashboard</Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Link to={`/${currentFestival.id}/exhibitors`} className='nav-link'>Réservation
+                            <Link to={`/${selectedFestival.id}/exhibitors`} className='nav-link'>Réservation
                                 exposant</Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Link to={`/${currentFestival.id}/games`} className='nav-link'>Réservation jeu</Link>
+                            <Link to={`/${selectedFestival.id}/games`} className='nav-link'>Réservation jeu</Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Link to={`/${currentFestival.id}/invoicing`} className='nav-link'>Facturation</Link>
+                            <Link to={`/${selectedFestival.id}/invoicing`} className='nav-link'>Facturation</Link>
                         </Nav.Item>
                     </Nav>
                 </Col>
@@ -63,4 +63,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default Navbar
