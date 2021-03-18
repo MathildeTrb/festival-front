97ec575708502c9461b1bbd4643a188b6a3cf49e
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2021-03-19 00:15:03 +0100

    add user in progress

---
+++ src/components/user/ModalAddUser.tsx
@@ -0,0 +1,105 @@
+import {Col, Container, Modal, Row} from "react-bootstrap";
+import {useState} from "react";
+import useToken from "../../utils/useToken";
+
+const ModalUpdateProfil = (props) => {
+
+
+    const [firstname, setFirstname] = useState<string>();
+    const [lastname, setLastname] = useState<string>();
+    const [mail, setMail] = useState<string>();
+    const [password, setPassword] = useState<string>();
+
+
+    function handleSubmit() {
+
+        const user = {
+            "firstname": firstname,
+            "lastname": lastname,
+            "mail": mail,
+            "password": password
+        }
+
+        props.addAccount(user)
+
+        props.onHide()
+    }
+
+    const handleChange = set => event => {
+        set(event.target.value)
+    }
+
+    return (
+        <Modal
+            {...props}
+            size="xl"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                     Ajout d'un bénévole
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form onSubmit={handleSubmit}>
+
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <label>Mail : </label>
+                                <input
+                                    className="mon-input"
+                                    type="text"
+                                    required
+                                    value={mail}
+                                    onChange={handleChange(setMail)}
+                                />
+                            </Col>
+                            <Col>
+                                <label>Prénom : </label>
+                                <input
+                                    className="mon-input"
+                                    type="text"
+                                    required
+                                    value={firstname}
+                                    onChange={handleChange(setFirstname)}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <label>Nom : </label>
+                                <input
+                                    className="mon-input"
+                                    type="text"
+                                    required
+                                    value={lastname}
+                                    onChange={handleChange(setLastname)}
+                                />
+                            </Col>
+                            <Col>
+                                <label>Mot de passe : </label>
+                                <input
+                                    className="mon-input"
+                                    type="password"
+                                    required
+                                    value={password}
+                                    onChange={handleChange(setPassword)}
+                                />
+                            </Col>
+                        </Row>
+                    </Container>
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <button className="mon-validate-button" onClick={handleSubmit}>
+                    validation
+                </button>
+            </Modal.Footer>
+        </Modal>
+    );
+
+}
+
+export default ModalUpdateProfil;
--- src/components/user/Users.tsx
+++ src/components/user/Users.tsx
@@ -2,14 +2,32 @@ import useAxios from "../../utils/useAxios";
 import {Col, Row} from "react-bootstrap";
 import useToken from "../../utils/useToken";
 import UsersRow from "./UsersRow";
+import {VscDiffAdded} from "react-icons/vsc"
+import {useState} from "react";
+import ModalAddUser from "./ModalAddUser";
+import {User} from "../../utils/types";
 
 const Users = () => {
 
     const {data: loadedUsers, setData: setLoadedUsers, isPending, error} = useAxios("users")
     const {isAdmin} = useToken()
 
+    const [modalShow, setModalShow] = useState(false)
+
     const handleDelete = user => {
         setLoadedUsers(loadedUsers.filter(value => value.id !== user.id))
+    }
+
+    const handleCreate = (user : User) => {
+
+    }
+
+    const handleShowModal = () => {
+        setModalShow(true)
+    }
+
+    const handleCloseModal = () => {
+        setModalShow(false)
     }
 
     return (
@@ -17,30 +35,36 @@ const Users = () => {
             {isPending && <p>Is loading ...</p>}
             {error && <p>{error}</p>}
             {loadedUsers &&
-            <Row>
-                <Col md={2}>
+                <div>
+                    <Row>
+                        <Col md={2}>
 
-                </Col>
-                <Col md={8}>
-                    <table className="table table-striped table-hover">
-                        <thead>
-                        <tr>
-                            <th scope="col">Nom</th>
-                            <th scope="col">Prénom</th>
-                            <th scope="col">mail</th>
-                            <th scope="col">fonction</th>
-                            {isAdmin() && <th scope="col"/>}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {loadedUsers.map((user, index) => <UsersRow key={index} user={user} onDelete={handleDelete}/>)}
-                        </tbody>
-                    </table>
-                </Col>
-                <Col md={8}>
+                        </Col>
+                        <Col md={8}>
+                            <table className="table table-striped table-hover">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Nom</th>
+                                    <th scope="col">Prénom</th>
+                                    <th scope="col">mail</th>
+                                    <th scope="col">fonction</th>
+                                    {isAdmin() && <th scope="col"/>}
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {loadedUsers.map((user, index) => <UsersRow key={index} user={user} onDelete={handleDelete}/>)}
+                                </tbody>
+                            </table>
+                        </Col>
+                        <Col md={8}>
 
-                </Col>
-            </Row>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <VscDiffAdded onClick={handleShowModal}/>
+                        <ModalAddUser show={modalShow} onHide={handleCloseModal} addAcount={handleCreate}/>
+                    </Row>
+                </div>
 
             }
         </div>
