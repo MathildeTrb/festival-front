5850292da5a21f60cefc0618826a49899917aad9
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2021-03-22 23:52:09 +0100

    calendar ok

--- src/components/exhibitorMonitoring/ExhibitorMonitoringRow.tsx
+++ src/components/exhibitorMonitoring/ExhibitorMonitoringRow.tsx
@@ -1,20 +1,75 @@
-import {FC} from "react";
+import {Dispatch, FC, SetStateAction, useState} from "react";
 import {ExhibitorMonitoring} from "../../utils/types";
+import {ImCalendar} from "react-icons/im"
+import Calendar from "react-calendar"
+import "../../css/sample.css"
+import CalendarModal from "./CalendarModal";
+import axios from "../../utils/axios";
+import useToken from "../../utils/useToken";
+import useAxios from "../../utils/useAxios";
 
-const ExhibitorMonitoringRow: FC<{ exhibitorMonitoring: ExhibitorMonitoring }> = ({exhibitorMonitoring}) => {
 
-    return (
+const ExhibitorMonitoringRow:
+    FC<{ exhibitorMonitoring: ExhibitorMonitoring }> =
+    ({exhibitorMonitoring}) => {
 
-        <tr>
-            <td>{exhibitorMonitoring.exhibitor.name}</td>
-            <td>{exhibitorMonitoring.dateContact1}</td>
-            <td>{exhibitorMonitoring.dateContact2}</td>
-            <td>{exhibitorMonitoring.dateContact3}</td>
-            <td>{exhibitorMonitoring.status.label}</td>
-            <td>{exhibitorMonitoring.reservation ? exhibitorMonitoring.reservation.id : "pas de résa"}</td>
-        </tr>
+        const {token} = useToken();
 
-    )
-}
+        const [date1, setDate1] = useState<Date>(exhibitorMonitoring.dateContact1 ? new Date(exhibitorMonitoring.dateContact1) : null);
+        const [date2, setDate2] = useState<Date>(exhibitorMonitoring.dateContact2 ? new Date(exhibitorMonitoring.dateContact2) : null);
+        const [date3, setDate3] = useState<Date>(exhibitorMonitoring.dateContact3 ? new Date(exhibitorMonitoring.dateContact3) : null);
+
+        const [showModal1, setShowModal1] = useState<boolean>(false);
+        const [showModal2, setShowModal2] = useState<boolean>(false);
+        const [showModal3, setShowModal3] = useState<boolean>(false);
+
+        const onValidate = async () => {
+            exhibitorMonitoring.dateContact1 = date1;
+            exhibitorMonitoring.dateContact2 = date2;
+            exhibitorMonitoring.dateContact3 = date3;
+            axios.post(
+                "exhibitorMonitorings",
+                {'exhibitorMonitoring': exhibitorMonitoring},
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + token
+                    }
+                }
+            );
+        }
+
+        console.log("Date1 "+ date1);
+        console.log("Date2 "+ date2);
+        console.log("Date3 "+ date3);
+
+        return (
+
+            <tr>
+                <td>{exhibitorMonitoring.exhibitor.name}</td>
+                <td>
+                    <ImCalendar onClick={() => setShowModal1(true)}/>
+                    <CalendarModal show={showModal1} onHide={() => setShowModal1(false)} date={date1} setDate={setDate1}
+                                   onValidate={onValidate}/>
+                    {exhibitorMonitoring.dateContact1 ? new Date(exhibitorMonitoring.dateContact1).toLocaleDateString() : " pas de 1er contact"}
+                </td>
+                <td>
+                    <ImCalendar onClick={() => setShowModal2(true)}/>
+                    <CalendarModal show={showModal2} onHide={() => setShowModal2(false)} date={date2} setDate={setDate2}
+                                   onValidate={onValidate}/>
+                    {exhibitorMonitoring.dateContact2 ? new Date(exhibitorMonitoring.dateContact2).toLocaleDateString() : " pas de 2nd contact"}
+                </td>
+                <td>
+                    <ImCalendar onClick={() => setShowModal3(true)}/>
+                    <CalendarModal show={showModal3} onHide={() => setShowModal3(false)} date={date3} setDate={setDate3}
+                                   onValidate={onValidate}/>
+                    {exhibitorMonitoring.dateContact3 ? new Date(exhibitorMonitoring.dateContact3).toLocaleDateString() : " pas de 3e contact"}
+                </td>
+                <td>{exhibitorMonitoring.status.label}</td>
+                <td>{exhibitorMonitoring.reservation ? exhibitorMonitoring.reservation.id : "pas de réservation"}</td>
+            </tr>
+
+        )
+    }
 
 export default ExhibitorMonitoringRow;
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,51 +1,19 @@
 import {useState} from "react";
-import axios from "../../utils/axios";
-import {Button, Form} from "react-bootstrap";
+import Calendar from "react-calendar"
+import "../../css/sample.css"
 
 const Home = () => {
 
-    const [file, setFile] = useState<File>();
-    const [isSent, setIsSent] = useState<boolean>(false)
+    const [date, setDate] = useState(new Date())
 
-    const handleChange = event => {
-        setFile(event.currentTarget.files[0])
-    }
-
-    const handleSubmit = event => {
-        event.preventDefault();
-
-        const formData: FormData = new FormData();
-        formData.append("file", file, file.name);
-
-        axios.post("games/photo", formData, {
-            headers: {
-                "Content-Type": "multipart/form-data"
-            }
-        })
-            .then(() => setIsSent(true))
-            .catch(err => console.log(err.message))
-    }
-
-    const handleClick = () => {
-        axios.post("games/test")
-            .then(({data}) => console.log(data))
+    const onChange = date => {
+        setDate(date)
     }
 
     return (
         <div className="container">
-            <Form onSubmit={handleSubmit}>
-                <div>
-                    <Form.File onChange={handleChange}/>
-                </div>
-
-                <div>
-                    <Button type="submit" variant="primary">Valider</Button>
-                </div>
-            </Form>
-
-            {isSent && <div>Envoyé</div>}
-
-            <button type="button" onClick={handleClick}>Click</button>
+            <Calendar onChange={onChange} value={date}/>
+            {console.log(date)}
         </div>
     )
 }
