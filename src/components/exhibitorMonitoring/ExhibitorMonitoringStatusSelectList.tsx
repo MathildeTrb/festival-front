c72107857faa38bba90922a312993e4aeb9a36b0
Guillaume DUFOUR <46460244+Guillaume-Dufour@users.noreply.github.com>
2021-03-23 00:24:58 +0100

    ExhibitorMonitoringStatusSelectList en cours

---
+++ src/components/exhibitorMonitoring/ExhibitorMonitoringStatusSelectList.tsx
@@ -0,0 +1,15 @@
+import {FC} from "react";
+import {ExhibitorMonitoring} from "../../utils/types";
+import {Form} from "react-bootstrap";
+
+const ExhibitorMonitoringStatusSelectList: FC<{selected?: ExhibitorMonitoring}> = () => {
+    return (
+        <>
+            <Form.Control as="select">
+
+            </Form.Control>
+        </>
+    )
+}
+
+export default ExhibitorMonitoringStatusSelectList;
